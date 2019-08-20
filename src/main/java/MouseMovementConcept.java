dec18e0fd05630e701654116ddbfb0d9036b658f
Aswathinpk <aswathi.npk@gmail.com>
2019-08-20 10:39:05 -0300

    updated Mouse movement code

--- src/main/java/MouseMovementConcept.java
+++ src/main/java/MouseMovementConcept.java
@@ -10,7 +10,7 @@ public class MouseMovementConcept
     public static void main(String[] args) throws InterruptedException
     {
         WebDriver driver=new ChromeDriver();
-        System.setProperty("webdriver.chrome.driver", "C:/Users/nimal/testOne/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:/Users/nimal/Automation-selenium/chromedriver.exe");
 
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
