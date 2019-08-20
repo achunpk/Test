36648d028c0ad1b62e3c496b062ae0f6122ec60b
Aswathinpk <aswathi.npk@gmail.com>
2019-08-20 10:41:17 -0300

    updated Navigation code

--- src/main/java/Navigation.java
+++ src/main/java/Navigation.java
@@ -6,7 +6,7 @@ import java.util.concurrent.TimeUnit;
 public class Navigation {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
-        System.setProperty("webdriver.chrome.driver", "C:/Users/nimal/testOne/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:/Users/nimal/Automation-selenium/chromedriver.exe");
         driver.manage().window().maximize(); //maximisie window
         driver.manage().deleteAllCookies(); //delete all cookies
         driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
