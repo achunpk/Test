f41e09ce710fa8aaa88779ff981e996d51bfee60
Aswathinpk <aswathi.npk@gmail.com>
2019-08-20 10:06:32 -0300

    updated Drag and drop code

--- src/main/java/DragandDrop.java
+++ src/main/java/DragandDrop.java
@@ -8,7 +8,7 @@ public class DragandDrop
     public static void main(String[] args)
     {
         WebDriver driver= new ChromeDriver();
-        System.setProperty("webdriver.chrome.driver", "C:/Users/nimal/testOne/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:/Users/nimal/Automation-selenium/chromedriver.exe");
 
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
